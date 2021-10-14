9516d702d46c0c286550d4a91f7c5560dbf067a7
Gourdon Joris <77497552+Joris07@users.noreply.github.com>
2021-10-14 13:53:13 +0200

    Update app.py

--- local/app.py
+++ local/app.py
@@ -35,7 +35,8 @@ def hello():
                 verif = False
                 alt = i.get('alt')  # Récupération de l'alt de l'image
                 src = i.get('src')  # Récupération du lien de l'image
-                if alt != "":
+                if alt != "" and alt != None and src != None and src != "":
+                    print(src)
                     # Ouvre le dictionnaire de données
                     with open('dico_'+phobie+'.json') as json_file:
                         data = json.load(json_file)
@@ -43,27 +44,26 @@ def hello():
                             for mot in data[language]:
                                 if mot in alt and data[language][mot] == 5:
                                     verif = True
-                                    z.update({src: 100})
-                                    # Mettre la stat (Ex: serpent=100%)
+                                    z.update({str(src): 100})# Mettre la stat (Ex: serpent=100%)
                                     break
 
-            ok = False
-            if verif == False:
-                avant = i.parent
-                # Tant qu'un mot déterminant à 100% la phobie n'est pas trouvé et que le corps à un parent
-                while(ok == False and avant != None):
-                    for t in avant.find_all(text=True):
-                        # Si le text est non-vide
-                        if t != "\n" and len(t) != 0 and t.parent.name not in blacklist:
-                            with open('dico_'+phobie+'.json') as json_file:
-                                mydata = json.load(json_file)
-                                for langue in mydata: #Pour chaque langue 
-                                    for word in mydata[langue]: # Compare les mots du dictionnaire avec le texte de la page
-                                        if word in t and mydata[langue][word] == 5:
-                                            ok = True  # Définir le fait qu'il y a la phobie à 100%
-                                            z.update({src: 100})
-                                            break
-                    avant = avant.parent
+                ok = False
+                if verif == False:
+                    avant = i.parent
+                    # Tant qu'un mot déterminant à 100% la phobie n'est pas trouvé et que le corps à un parent
+                    while(ok == False and avant != None):
+                        for t in avant.find_all(text=True):
+                            # Si le text est non-vide
+                            if src != None and t != "\n" and len(t) != 0 and t.parent.name not in blacklist:
+                                with open('dico_'+phobie+'.json') as json_file:
+                                    mydata = json.load(json_file)
+                                    for langue in mydata: #Pour chaque langue 
+                                        for word in mydata[langue]: # Compare les mots du dictionnaire avec le texte de la page
+                                            if word in t and mydata[langue][word] == 5:
+                                                ok = True  # Définir le fait qu'il y a la phobie à 100%
+                                                z.update({str(src): 100})
+                                                break
+                        avant = avant.parent
             fg.update({phobie:z})
     return jsonify(fg)
 
