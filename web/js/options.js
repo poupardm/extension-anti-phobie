cd1c28707c0c5ef99b4d9ce40b1775e9e4e35634
poupardm <mpoupard.pro@gmail.com>
2021-10-19 15:19:51 +0200

    Extension WEB : - Ajout/suppression de site dans la whitelist/blacklist

--- web/js/options.js
+++ web/js/options.js
@@ -1,122 +1,244 @@
 // Load text from locale files
 document.title = chrome.i18n.getMessage("optionPageTitle");
 document.querySelector("#optionsTitle").innerHTML = chrome.i18n.getMessage("optionPageTitle");
-//document.querySelector("#settingsText").innerHTML = chrome.i18n.getMessage("settings");
-document.querySelector("#typeOfPhobiaText").innerHTML = chrome.i18n.getMessage("typeOfPhobiaText");
-document.querySelector("#araText").innerHTML = chrome.i18n.getMessage("araText");
-document.querySelector("#reptText").innerHTML = chrome.i18n.getMessage("reptText");
 document.querySelector("#saveButtonText").innerHTML = chrome.i18n.getMessage("save");
-document.querySelector(".tab-general").classList.add("hideTab");
-document.querySelector(".tab-blacklist").classList.add("hideTab");
 
 
 // ### Extension switch on click event ###
 document.getElementById("whitelist-link").addEventListener("click", function () {
-    selectedMenu("whitelist")
+    loadSelectedMenu("#whitelist")
 });
 document.getElementById("general-link").addEventListener("click", function () {
-    selectedMenu("general")
+    loadSelectedMenu("#general")
 });
 document.getElementById("blacklist-link").addEventListener("click", function () {
-    selectedMenu("blacklist")
-});
-
-/** Select and change the menu display
- *      - Params : string linkName
- **/
-function selectedMenu(linkName) {
-    var tabGeneral = document.querySelector(".tab-general");
-    var tabWhitelist = document.querySelector(".tab-whitelist");
-    var tabBlacklist = document.querySelector(".tab-blacklist");
-    switch (linkName) {
-        case "whitelist":
-            var whitelistLink = document.getElementById("whitelist-link");
-            document.getElementById("general-link").classList.remove("active");
-            document.getElementById("blacklist-link").classList.remove("active");
-            whitelistLink.className = "active";
-            if(!tabGeneral.classList.contains("hideTab")) tabGeneral.classList.add("hideTab");
-            if(!tabBlacklist.classList.contains("hideTab")) tabBlacklist.classList.add("hideTab");
-            if(tabWhitelist.classList.contains("hideTab")) tabWhitelist.classList.remove("hideTab");
-            break;
-        case "blacklist":
-            var blacklistLink = document.getElementById("blacklist-link");
-            document.getElementById("general-link").classList.remove("active");
-            document.getElementById("whitelist-link").classList.remove("active");
-            blacklistLink.className = "active";
-            if(!tabGeneral.classList.contains("hideTab")) tabGeneral.classList.add("hideTab");
-            if(tabBlacklist.classList.contains("hideTab")) tabBlacklist.classList.remove("hideTab");
-            if(!tabWhitelist.classList.contains("hideTab")) tabWhitelist.classList.add("hideTab");
-            break;
-        default:
-            var generalLink = document.getElementById("general-link");
-            document.getElementById("blacklist-link").classList.remove("active");
-            document.getElementById("whitelist-link").classList.remove("active");
-            generalLink.className = "active";
-            if(tabGeneral.classList.contains("hideTab")) tabGeneral.classList.remove("hideTab");
-            if(!tabBlacklist.classList.contains("hideTab")) tabBlacklist.classList.add("hideTab");
-            if(!tabWhitelist.classList.contains("hideTab")) tabWhitelist.classList.add("hideTab");
-            break;
-    }
-}
-
-/** Remove a website from the whitelist
- *      - Event type : onclick
-**/
-document.getElementById("removeButtonWhitelist").addEventListener("click", function () {
-    // For each website in the list do
-    var whitelistDropdown = document.querySelector("#whitelistDropdown");
-    Array.from(whitelistDropdown.options).forEach(function(option_element) {
-        // Get information from the current value
-        if(option_element.selected && option_element.value != 0 && confirm("Êtes-vous sûr(e) de vouloir supprimer '" + option_element.text + "' de la liste blanche ?")) whitelistDropdown.remove(whitelistDropdown.selectedIndex);
-        if (whitelistDropdown.length === 1) { whitelistDropdown[0].text = "Aucun site dans la liste..."; whitelistDropdown.setAttribute("disabled", "true");}
-    });
+    loadSelectedMenu("#blacklist")
 });
 
 // ### Window load event ###
 window.addEventListener("load", function () {
-    // Get state of the extension switch
-    chrome.storage.sync.get('extensionButton', function(data) {
-        document.querySelector("#extensionButton").checked = data.extensionButton
-        // Set the text of the extension button
-        let extensionButtonText = document.getElementById("extensionButtonText");
-        if(data.extensionButton) extensionButtonText.innerText = chrome.i18n.getMessage("disableExtText");
-        else extensionButtonText.innerText = chrome.i18n.getMessage("enableExtText");
-    });
-
-    // Set state of the arachnophobia checkbox
-    chrome.storage.sync.get('araCheckbox', function(data) {
-        document.querySelector("#araCheckbox").checked = data.araCheckbox
-    });
-
-    // Set state of the reptile phobia checkbox
-    chrome.storage.sync.get('reptCheckbox', function(data) {
-        document.querySelector("#reptCheckbox").checked = data.reptCheckbox
-    });
+    loadSelectedMenu(location.hash);
 });
 
-/*
-// ### Extension switch on click event ###
-var wtButton = document.getElementById("wtButton");
-wtButton.addEventListener("click", function () {
-    var tab = new Array("https://google.fr", "https://google.com");
-    var nb = tab.push("http://banane.fr/");
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log(tabs[0].url);
-        chrome.storage.sync.set({
-            activeTabUrl: tab.push(tabs[0].url)
+/** Select, load and change the menu display
+ *      - Params : string locationHash
+ **/
+function loadSelectedMenu(locationHash) {
+    // Get menus link
+    var tabGeneral = document.querySelector(".tab-general");
+    var tabWhitelist = document.querySelector(".tab-whitelist");
+    var tabBlacklist = document.querySelector(".tab-blacklist");
+    switch (locationHash) {
+        // If the whitelist menu is selected
+        case "#whitelist":
+            // Set the whitelist item menu active in the sidebar
+            document.getElementById("general-link").classList.remove("active");
+            document.getElementById("blacklist-link").classList.remove("active");
+            document.getElementById("whitelist-link").className = "active";
+            // Display the great div for the whitelist tab and hide other tab
+            if(!tabGeneral.classList.contains("hideTab")) tabGeneral.classList.add("hideTab");
+            if(!tabBlacklist.classList.contains("hideTab")) tabBlacklist.classList.add("hideTab");
+            if(tabWhitelist.classList.contains("hideTab")) tabWhitelist.classList.remove("hideTab");
+            // Load the whitelist dropdown
+            loadListDropdown(document.querySelector("#whitelistDropdown"),"whitelist");
+            break;
+        // If the blacklist menu is selected
+        case "#blacklist":
+            // Set the blacklist item menu active in the sidebar
+            document.getElementById("general-link").classList.remove("active");
+            document.getElementById("whitelist-link").classList.remove("active");
+            document.getElementById("blacklist-link").className = "active";
+            // Display the great div for the blacklist tab and hide other tab
+            if(!tabGeneral.classList.contains("hideTab")) tabGeneral.classList.add("hideTab");
+            if(tabBlacklist.classList.contains("hideTab")) tabBlacklist.classList.remove("hideTab");
+            if(!tabWhitelist.classList.contains("hideTab")) tabWhitelist.classList.add("hideTab");
+            // Load the blacklist dropdown
+            loadListDropdown(document.querySelector("#blacklistDropdown"),"blacklist");
+            break;
+        // If other menu is selected
+        default:
+            // Set the general item menu active in the sidebar
+            document.getElementById("blacklist-link").classList.remove("active");
+            document.getElementById("whitelist-link").classList.remove("active");
+            document.getElementById("general-link").className = "active";
+            // Display the great div for the general tab and hide other tab
+            if(tabGeneral.classList.contains("hideTab")) tabGeneral.classList.remove("hideTab");
+            if(!tabBlacklist.classList.contains("hideTab")) tabBlacklist.classList.add("hideTab");
+            if(!tabWhitelist.classList.contains("hideTab")) tabWhitelist.classList.add("hideTab");
+            // Load and set the text from language file to general tab
+            document.querySelector("#typeOfPhobiaText").innerHTML = chrome.i18n.getMessage("typeOfPhobiaText");
+            document.querySelector("#araText").innerHTML = chrome.i18n.getMessage("araText");
+            document.querySelector("#reptText").innerHTML = chrome.i18n.getMessage("reptText");
+
+            // Get state of the extension switch
+            chrome.storage.sync.get('extensionButton', function(data) {
+                document.querySelector("#extensionButton").checked = data.extensionButton
+                // Set the text of the extension button
+                let extensionButtonText = document.getElementById("extensionButtonText");
+                if(data.extensionButton) extensionButtonText.innerText = chrome.i18n.getMessage("disableExtText");
+                else extensionButtonText.innerText = chrome.i18n.getMessage("enableExtText");
+            });
+
+            // Set state of the arachnophobia checkbox
+            chrome.storage.sync.get('araCheckbox', function(data) {
+                document.querySelector("#araCheckbox").checked = data.araCheckbox
+            });
+
+            // Set state of the reptile phobia checkbox
+            chrome.storage.sync.get('reptCheckbox', function(data) {
+                document.querySelector("#reptCheckbox").checked = data.reptCheckbox
+            });
+            break;
+    }
+}
+
+/** onclick Event listener of add and remove button of whitelist/blacklist tab **/
+document.getElementById("removeButtonWhitelist").addEventListener("click", function () {removeInList(document.querySelector("#whitelistDropdown"),"whitelist");});
+document.getElementById("removeButtonBlackList").addEventListener("click", function () {removeInList(document.querySelector("#blacklistDropdown"),"blacklist");});
+document.getElementById("addButtonWhitelist").addEventListener("click", function () {addInList("whitelist");});
+document.getElementById("addButtonBlackList").addEventListener("click", function () {addInList("blacklist");});
+
+/** Add a website to white/black-list
+*      - Params : string type
+**/
+function addInList(type) {
+    // Get website input
+    var websiteInput = document.getElementById("input-"+type)
+    // If the pattern of the website input is valid and the value of the input not equals to ""
+    if(websiteInput.checkValidity() && websiteInput.value != "") {
+        var tab = new Array();
+        var isInList = false;
+        // If type is whitelist
+        if(type == "whitelist") {
+            // Get all data from whitelist
+            chrome.storage.local.get("whitelist", function (data) {
+                tab = data.whitelist;
+                // Check if the new input already exist in the whitelist
+                tab.forEach(function (itemTab) {
+                    data.whitelist.forEach(function (itemWhitelist) {
+                        //console.log("itemWhitelist = " + itemWhitelist + ", itemTab = " + itemTab);
+                        if (itemWhitelist == websiteInput.value && itemTab == websiteInput.value) {
+                            isInList = true;
+                        }
+                    });
+                });
+                // If the new input is not whitelisted -> add in whitelist
+                if (!isInList) {
+                    tab.push(websiteInput.value);
+                    chrome.storage.local.set({
+                        whitelist: tab
+                    });
+                }
+            });
+            // Load whitelist in dropdown
+            loadListDropdown(document.querySelector("#whitelistDropdown"), "whitelist");
+        } else { // Else is blacklist
+            // Get all data from blacklist
+            chrome.storage.local.get("blacklist", function (data) {
+                tab = data.blacklist;
+                // Check if the new input already exist in the blacklist
+                tab.forEach(function (itemTab) {
+                    data.blacklist.forEach(function (itemBlacklist) {
+                        if (itemBlacklist == websiteInput.value && itemTab == websiteInput.value) {
+                            isInList = true;
+                        }
+                    });
+                });
+                // If the new input is not blacklisted -> add in blacklist
+                if (!isInList) {
+                    tab.push(websiteInput.value);
+                    chrome.storage.local.set({
+                        blacklist: tab
+                    });
+                }
+            });
+            // Load blacklist in dropdown
+            loadListDropdown(document.querySelector("#blacklistDropdown"), "blacklist");
+        }
+    }
+}
+
+/** Remove a website to white/black-list
+ *      - Params : object dropdown
+ *                 string type
+ **/
+function removeInList(dropdown, type) {
+    // For each website in the list do
+    Array.from(dropdown.options).forEach(function(option_element) {
+        // If the current option is selected, the value not equal at 0 and the confirm was true then
+        if(option_element.selected && option_element.value != 0 && confirm("Êtes-vous sûr(e) de vouloir supprimer '" + option_element.text + " de cette liste ?")) {
+            var tab = new Array();
+            // Get all data from param type list
+            chrome.storage.local.get(type, function(data) {
+                if(type == "whitelist") tab = data.whitelist;
+                else if(type == "blacklist") tab = data.blacklist;
+
+                // Removing the element from local list
+                for(var i = 0; i < tab.length; i++){
+                    if ( tab[i] === option_element.text) {
+                        tab.splice(i, 1);
+                    }
+                }
+                // Set the new list in storage
+                if(type == "whitelist") {
+                    chrome.storage.local.set({
+                        whitelist: tab
+                    });
+                } else if(type == "blacklist"){
+                    chrome.storage.local.set({
+                        blacklist: tab
+                    });
+                }
+            });
+        }
+    });
+    // Load the list in dropdown
+    loadListDropdown(dropdown, type);
+}
+
+
+/** Load website in whitelist dropdown from storage **/
+function loadListDropdown(dropdown, type) {
+    // Clear all things except the first one
+    var i, L = dropdown.options.length - 1;
+    for(i = L; i > 0; i--) {
+        dropdown.remove(i);
+    }
+    // If is whitelist
+    if (type == "whitelist") {
+        // Get all data from whitelist and add it in dropdown
+        chrome.storage.local.get('whitelist', function (data) {
+            data.whitelist.forEach(function (item, index, array) {
+                let newOption = new Option(item, index + 1);
+                dropdown.add(newOption, undefined);
+            });
+            // If the whitelist don't have website display it in dropdown
+            if (data.whitelist.length == 0) {
+                dropdown[0].text = "Aucun site dans la liste...";
+                dropdown.setAttribute("disabled", "true");
+            } else {
+                dropdown[0].text = "Choisissez un site...";
+                dropdown.removeAttribute("disabled");
+            }
         });
-    });
-    tab.forEach(function(item, index, array) {
-        console.log(item, index);
-    });
-
-    chrome.storage.sync.get('activeTabUrl', function(data) {
-        console.log(data);
-    });
-
-});
-*/
-
+    } else if(type == "blacklist") {
+        // Get all data from blacklist and add it in dropdown
+        chrome.storage.local.get('blacklist', function (data) {
+            data.blacklist.forEach(function (item, index, array) {
+                let newOption = new Option(item, index + 1);
+                dropdown.add(newOption, undefined);
+            });
+            // If the blacklist don't have website display it in dropdown
+            if (data.blacklist.length == 0) {
+                dropdown[0].text = "Aucun site dans la liste...";
+                dropdown.setAttribute("disabled", "true");
+            } else {
+                dropdown[0].text = "Choisissez un site...";
+                dropdown.removeAttribute("disabled");
+            }
+        });
+    }
+}
 
 // ### Extension switch on click event ###
 var extensionButton = document.getElementById("extensionButton");
