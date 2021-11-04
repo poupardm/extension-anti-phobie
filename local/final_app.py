7aa77f050e682076fa018a9a77eca6c4446c9ae1
Gourdon Joris <77497552+Joris07@users.noreply.github.com>
2021-11-04 14:24:31 +0100

    Update final_app.py

--- local/final_app.py
+++ local/final_app.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as bs
 import requests
 import json
 import os, shutil
+import os.path
 from imageai.Classification import ImageClassification
 
 blacklist = [
@@ -21,25 +22,47 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 def is_downloadable(url, link):
-    if 'http' not in url and 'https' not in url and url[:2] == "//":
-        url="https:"+url
+    if ".svg" not in url:
+        if 'http' not in url and 'https' not in url and url[:2] == "//":
+            url="https:"+url
+            return url
+        elif 'http' not in url and 'https' not in url and url[:1] == "/":
+            pos = link.count("/") - 2
+            url= link.rsplit('/', pos)[0]+url
+            return url
+        try:
+            h = requests.head(url, allow_redirects=True)
+            header = h.headers
+            content_type = header.get('content-type')
+        except:
+            return False
+        if 'text' in content_type.lower():
+            return False
+        if 'html' in content_type.lower():
+            return False
         return url
-    elif 'http' not in url and 'https' not in url and url[:1] == "/":
-        pos = link.count("/") - 2
-        url= link.rsplit('/', pos)[0]+url
-        return url
-    try:
-        h = requests.head(url, allow_redirects=True)
-        header = h.headers
-        content_type = header.get('content-type')
-    except:
-        shutil.rmtree("image_dl")
+    else:
         return False
-    if 'text' in content_type.lower():
-        return False
-    if 'html' in content_type.lower():
-        return False
-    return url
+
+def immaga_prediction(url_image, nb_object):
+    api_key = "acc_bd445a4e53dfa84"
+    api_secret = "19885794472fea086877c8edb236840f"
+
+    response = requests.get(
+        "https://api.imagga.com/v2/tags?image_url=%s" % url_image,
+        auth=(api_key, api_secret))
+
+    reponse = response.json()
+    for value in reponse['result']['tags']:
+        i = 0
+        immaga_tab = json.loads("[ ]")
+        for value in reponse['result']['tags']:
+            if i < nb_object:
+                immaga_tab.append({"word" : value['tag']['en'], "score" : value['confidence']})
+            else:
+                break
+            i+=1
+        return immaga_tab
 
 @app.route("/api/phobie")
 def hello():
@@ -115,32 +138,37 @@ def hello():
                                                         break
                         avant = avant.parent
                 if verif == False and ok == False and {"url" : str(src), "score" : percent} not in tableau1:
-                    os.makedirs("image_dl")
                     lien = src
                     lien = is_downloadable(lien, no1)
-                    if(lien != None and lien != "" and lien != False):
-                        filename = ("image_dl/"+lien.rsplit('/', 1)[1])
-                        r = requests.get(lien, allow_redirects=True)
-                        open(filename, 'wb').write(r.content)
-                        list = os.listdir('image_dl')
-                        predict_score = json.loads('[ ]')
-                        if list:
-                            execution_path = os.getcwd()
-                            prediction = ImageClassification()
-                            prediction.setModelTypeAsResNet50()
-                            prediction.setModelPath( execution_path + "/modeles/resnet50_imagenet_tf.2.0.h5")
-                            prediction.loadModel()
+                    if lien != None and lien != "" and lien != False:
+                        if phobie != "coulrophobie":
+                            if os.path.exists("image_dl") == False:
+                                os.makedirs("image_dl")
+                            filename = ("image_dl/"+lien.rsplit('/', 1)[1])
+                            r = requests.get(lien, allow_redirects=True)
+                            open(filename, 'wb').write(r.content)
+                            list = os.listdir('image_dl')
+                            predict_score = json.loads('[ ]')
+                            if list:
+                                execution_path = os.getcwd()
+                                prediction = ImageClassification()
+                                prediction.setModelTypeAsResNet50()
+                                prediction.setModelPath( execution_path + "/modeles/resnet50_imagenet_tf.2.0.h5")
+                                prediction.loadModel()
 
-                            for fichier in list:
-                                if "png" in fichier or "jpg" in fichier:
-                                    try:
-                                        predictions, percentage_probabilities = prediction.classifyImage(("image_dl/"+fichier), result_count=3)
-                                        for index in range(len(predictions)):
-                                            predict_score.append({ "score" : percentage_probabilities[index],"word" : predictions[index]})
-                                        tableau1.append({"score" : predict_score,"url" : str(src) })
-                                    except ValueError:
-                                        continue;
-                        shutil.rmtree("image_dl")
+                                for fichier in list:
+                                    if "png" in fichier or "jpg" in fichier:
+                                        try:
+                                            predictions, percentage_probabilities = prediction.classifyImage(("image_dl/"+fichier), result_count=3)
+                                            for index in range(len(predictions)):
+                                                predict_score.append({ "score" : percentage_probabilities[index],"word" : predictions[index]})
+                                            tableau1.append({"score" : predict_score,"url" : str(src) })
+                                        except:
+                                            continue;
+                            if os.path.exists("image_dl"):
+                                shutil.rmtree("image_dl")
+                        else:
+                            tableau1.append({"score" : immaga_prediction(lien, 3),"url" : str(src) })
                 words_tab.clear
         final_data.append({phobie : tableau1})
     return jsonify(final_data)
